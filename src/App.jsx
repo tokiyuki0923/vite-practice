a16d4b743842ab4116bef03e96e834fa82032752
濱田時行 <hamadatokiyuki@hamadatokiyukinoMacBook-Pro.local>
2024-05-20 20:05:16 +0900

    すでに存在するdivタグの中にpokeAPIのフシギダネの画像を埋め込むことに成功

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,10 @@ import './App.css';
 import Pokemon from"./Pokemon.jsx";
 
 function App() {
-  const app = [];
   return (
   <>
-    <Pokemon />
     <div id='onetofive'>
-      <div id='one' className='pokemon'></div>
+      <div id='one' className='pokemon'><Pokemon /></div>
       <div id='two' className='pokemon'></div>
       <div id='three' className='pokemon'></div>
       <div id='four' className='pokemon'></div>
--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
 const Pokemon = () => {
+const [imgUrl, setImgUrl] = useState('');
+
+useEffect(() => {
+    const fetchPokemon = async () => {
+    try {
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon/1');
+        const data = await response.json();
+        const spriteUrl = data.sprites.other.dream_world.front_default;
+        setImgUrl(spriteUrl);
+    } catch (error) {
+        console.error('Error fetching Pokemon data:', error);
+    }
+    };
+    fetchPokemon();
+}, []);
+
 return (
-    <div>
-        
+    <div id="one">
+    {imgUrl && <img src={imgUrl}/>}
     </div>
-)
-}
+);
+};
+
 
 export default Pokemon

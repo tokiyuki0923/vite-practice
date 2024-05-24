5b4071c5f6b73a625868a5fb25b85a5c56e6c31e
濱田時行 <hamadatokiyuki@hamadatokiyukinoMacBook-Pro.local>
2024-05-24 16:32:19 +0900

    エラーが起きててアップできない現象

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <div id='onetofive'>
       <div id='one' className='pokemon'><Pokemon /></div>
       <div id='two' className='pokemon'><Pokemon /></div>
-      <div id='three' className='pokemon'><Pokemon /></div>
+      <div id='three' className='pokemon'></div>
       <div id='four' className='pokemon'></div>
       <div id='five' className='pokemon'></div>
     </div>
